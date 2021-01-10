2433ded83095a6601982fb3622d704df8feff89b
amaan18 <amaangokak18@gmail.com>
2021-01-11 02:28:14 +0530

    might set up top level await

---
+++ dist/constants.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PROD = void 0;
+exports.PROD = process.env.NODE_ENV === "production";
+//# sourceMappingURL=constants.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("pls");
+import { MikroORM } from "@mikro-orm/core";
+import { PROD } from "./constants";
+import { Post } from "./entities/post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: "graphql-ts-server",
+    debug: !PROD,
+    type: "postgresql",
+    entities: [Post],
+  });
+};
